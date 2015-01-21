354a4616e92b34c825c145578839e1ff150f926e
Nick Blackwell <nickblackwell82@gmail.com>
2015-01-21 13:23:47 -0800

    moved parser into own file

---
+++ nas/mod-status-parser.js
@@ -0,0 +1,84 @@
+module.exports = {
+		parse: parse
+}
+
+
+function parse(page){
+
+var modeTable={"_":"waiting", "S":"starting", "R":"reading",
+		"W":"sending", "K":"keep-alive", "D":"dns-lookup",
+		"C":"closing", "L":"logging", "G":"gracefully-finishing",
+		"I":"idle-cleanup", ".":"open"};
+
+var ips=[];
+var modes=[];
+var hosts=[];
+
+var active=[];	
+var unique=[];
+
+var tables=page.split('<table'); tables.shift();
+
+var rows=tables[0].split('<tr>'); rows.shift(); //discard first
+
+rows.forEach(function(r, i){
+	if(i==0)return; //skip header
+	var row=r.split('</tr>')[0];
+	var tdatas=row.split('<td'); tdatas.shift();
+	
+	//console.log(r.split('</tr>')[0]);
+	
+	if(i==1){
+		
+		tdatas.forEach(function(t,i){
+			console.log(i+': '+t);
+		});
+		
+	}
+		
+		
+			
+	
+	
+	var ip=tdatas[10]; 
+	ip=ip.split('</td>')[0].substring(ip.indexOf('>')+1);
+	ips.push(ip);
+	
+	if(unique.indexOf(ip)==-1){
+		unique.push(ip);
+	}
+	
+	var mode=tdatas[3];
+
+	mode=mode.split('</td>')[0].substring(mode.indexOf('>')+1).replace(/^\s+|\s+$/g,'');
+	mode=mode.split('</b>')[0];
+	mode=mode.split('<b>').pop();
+	modes.push(modeTable[mode]);
+	
+	
+	var host=tdatas[11].split('</td>')[0].split('nowrap>').pop();
+	hosts.push(host);
+	//console.log(mode+' '+modeTable[mode]);	
+			
+		
+		
+	//}
+	
+});
+
+
+
+
+modes.forEach(function(m,i){
+	
+	
+	
+	if((['open']).indexOf(m)>=0)return;
+	active.push(i);
+	
+	
+})
+console.log('Active Slots: '+JSON.stringify(active));
+console.log('Unique Addrs: '+JSON.stringify(unique)+"\n");
+
+};
--- nas/nas.js
+++ nas/nas.js
@@ -138,83 +138,9 @@ var modStatusDialog=[['modStatusUrl', 'Url for mod-status? ', function(url){
 		
 			
 			var parsePage=function(page){
-				
-				var modeTable={"_":"waiting", "S":"starting", "R":"reading",
-						"W":"sending", "K":"keep-alive", "D":"dns-lookup",
-						"C":"closing", "L":"logging", "G":"gracefully-finishing",
-						"I":"idle-cleanup", ".":"open"};
-				
-				var ips=[];
-				var modes=[];
-				
-				var active=[];
-				
-				var unique=[];
-				
-				var tables=page.split('<table'); tables.shift();
-				
-				var rows=tables[0].split('<tr>'); rows.shift(); //discard first
-				
-				rows.forEach(function(r, i){
-					if(i==0)return; //skip header
-					var row=r.split('</tr>')[0];
-					var tdatas=row.split('<td'); tdatas.shift();
-					
-					//console.log(r.split('</tr>')[0]);
-					
-					if(i==1){
-						
-						tdatas.forEach(function(t,i){
-							console.log(i+': '+t);
-						});
-						
-					}
-						
-						
-							
-					
-					
-					var ip=tdatas[10]; 
-					ip=ip.split('</td>')[0].substring(ip.indexOf('>')+1);
-					ips.push(ip);
-					
-					if(unique.indexOf(ip)==-1){
-						unique.push(ip);
-					}
-					
-					var mode=tdatas[3];
-	
-					mode=mode.split('</td>')[0].substring(mode.indexOf('>')+1).replace(/^\s+|\s+$/g,'');
-					mode=mode.split('</b>')[0];
-					mode=mode.split('<b>').pop();
-					modes.push(modeTable[mode]);
-					
-					//console.log(mode+' '+modeTable[mode]);	
-							
-						
-						
-					//}
-					
-				});
-				
-				
-				
-				
-				modes.forEach(function(m,i){
-					
-					
-					
-					if((['open']).indexOf(m)>=0)return;
-					active.push(i);
-					
-					
-				})
-				console.log('Active Slots: '+JSON.stringify(active));
-				console.log('Unique Addrs: '+JSON.stringify(unique)+"\n");
-				
-			};
 			
-			parsePage(page);
+			require('./mod-status-parser.js').parse(page);
+			
 			setInterval(function(){
 				
 				
@@ -227,7 +153,7 @@ var modStatusDialog=[['modStatusUrl', 'Url for mod-status? ', function(url){
 							page+=chunk;
 						});
 						res.on('end', function (chunk) {
-							parsePage(page);
+							require('./mod-status-parser.js').parse(page);
 						});
 					}
 
